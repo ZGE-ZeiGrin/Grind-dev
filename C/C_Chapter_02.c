85af7c5a5474ce09eb3070fe680af7f1d42cfe8a
ZeiGrin <buiphuoctoan707@gmail.com>
2026-04-11 20:52:23 +0700

    Complete Exercise 2 chapter 2 .ZGE

--- C/C_Chapter_02.c
+++ C/C_Chapter_02.c
@@ -6,9 +6,43 @@ int main(){
 
     // Exercise 1: Write a program to input three positive integers from the keyboard. Find and display the maximum value among those three numbers.
 
+    // int a = 0;
+    // int b = 0;
+    // int c = 0;
+
+    // printf("Enter positive integer A: ");
+    // scanf("%d", &a);
+
+    // printf("Enter positive integer B: ");
+    // scanf("%d", &b);
+
+    // printf("Enter positive integer C: ");
+    // scanf("%d", &c);
+
+    // int max = a;
+
+    // if(max = a){
+    //     printf("Max value is: %d\n", max);
+    // }
+
+    // else if(b >= max){
+    //     printf("Max value is: %d\n", max);
+    // }
+
+    // else if(c >= max){
+    //     printf("Max value is: %d\n", max);
+    // }
+    
+    // else{
+    //     printf("Invalid value, please try again.\n");
+    // }
+
+    // Exercise 2: Write a program to input four positive integers from the keyboard. Find and display the maximum and minimum values among those four numbers.
+
     int a = 0;
     int b = 0;
     int c = 0;
+    int d = 0;
 
     printf("Enter positive integer A: ");
     scanf("%d", &a);
@@ -19,23 +53,29 @@ int main(){
     printf("Enter positive integer C: ");
     scanf("%d", &c);
 
+    printf("Enter positive integer D: ");
+    scanf("%d", &d);
+
     int max = a;
 
-    if(max = a){
-        printf("Max value is: %d\n", max);
-    }
+    if(b > max) max = b;
 
-    else if(b >= max){
-        printf("Max value is: %d\n", max);
-    }
+    if(c > max) max = c;
 
-    else if(c >= max){
-        printf("Max value is: %d\n", max);
-    }
-    
-    else{
-        printf("Invalid value, please try again.\n");
-    }
+    if(d > max) max = d;
+
+    printf("Max value is: %d\n", max);
+
+    int min = a;
+
+    if(b < min) min = b;
+
+    if(c < min) min = c;
+
+    if(d < min) min = d;
+
+    printf("Min value is: %d\n", min);
+
 
     return 0;
 }
