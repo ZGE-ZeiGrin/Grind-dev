bf6dbd4292a234f3be48f48941d55e66437239b6
ZeiGrin <buiphuoctoan707@gmail.com>
2026-04-21 21:30:41 +0700

    Day 7 C

--- C/Day7.c
+++ C/Day7.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+void happyBirthday(){
+
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday dear %s!");
+    printf("\nYou are %d years old!\n");
+}
+
 int main(){
 
-    printf("Take a break today.");
+    char name[] = "Grind";
+    int age = 19;
+
+    happyBirthday();
 
     return 0;
 }
