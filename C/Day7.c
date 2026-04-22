f09d88dfa33cfa01ae31aefe6dfe5186d7b2c6e3
ZeiGrin <buiphuoctoan707@gmail.com>
2026-04-22 23:28:52 +0700

    Update Day 7 learning C programming

--- C/Day7.c
+++ C/Day7.c
@@ -1,6 +1,9 @@
+// DAY 7 LEARNING C PROGRAMMING
 #include <stdio.h>
 
 void happyBirthday(){
+
+    // LESSON 16: Functions
 
     printf("\nHappy birthday to you!");
     printf("\nHappy birthday to you!");
