3a2398524574196511f0822a8570a77ae4cf4730
ZeiGrin <buiphuoctoan707@gmail.com>
2026-04-23 10:07:34 +0700

    Update ClassE_9_4_2026; C_Chapter_02; ClassE_23_4_2026

--- C/C_Chapter_02.c
+++ C/C_Chapter_02.c
@@ -6,137 +6,137 @@ int main(){
 
     // Exercise 1: Write a program to input three positive integers from the keyboard. Find and display the maximum value among those three numbers.
 
-    // int a = 0;
-    // int b = 0;
-    // int c = 0;
+    int a = 0;
+    int b = 0;
+    int c = 0;
 
-    // printf("Enter positive integer A: ");
-    // scanf("%d", &a);
+    printf("Enter positive integer A: ");
+    scanf("%d", &a);
 
-    // printf("Enter positive integer B: ");
-    // scanf("%d", &b);
+    printf("Enter positive integer B: ");
+    scanf("%d", &b);
 
-    // printf("Enter positive integer C: ");
-    // scanf("%d", &c);
+    printf("Enter positive integer C: ");
+    scanf("%d", &c);
 
-    // int max = a;
+    int max = a;
 
-    // if(max = a){
-    //     printf("Max value is: %d\n", max);
-    // }
+    if(max = a){
+        printf("Max value is: %d\n", max);
+    }
 
-    // else if(b >= max){
-    //     printf("Max value is: %d\n", max);
-    // }
+    else if(b >= max){
+        printf("Max value is: %d\n", max);
+    }
 
-    // else if(c >= max){
-    //     printf("Max value is: %d\n", max);
-    // }
+    else if(c >= max){
+        printf("Max value is: %d\n", max);
+    }
     
-    // else{
-    //     printf("Invalid value, please try again.\n");
-    // }
+    else{
+        printf("Invalid value, please try again.\n");
+    }
 
     // Exercise 2: Write a program to input four positive integers from the keyboard. Find and display the maximum and minimum values among those four numbers.
 
-    // int a = 0;
-    // int b = 0;
-    // int c = 0;
-    // int d = 0;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
 
-    // printf("Enter positive integer A: ");
-    // scanf("%d", &a);
+    printf("Enter positive integer A: ");
+    scanf("%d", &a);
 
-    // printf("Enter positive integer B: ");
-    // scanf("%d", &b);
+    printf("Enter positive integer B: ");
+    scanf("%d", &b);
 
-    // printf("Enter positive integer C: ");
-    // scanf("%d", &c);
+    printf("Enter positive integer C: ");
+    scanf("%d", &c);
 
-    // printf("Enter positive integer D: ");
-    // scanf("%d", &d);
+    printf("Enter positive integer D: ");
+    scanf("%d", &d);
 
-    // int max = a;
+    int max = a;
 
-    // if(b > max) max = b;
+    if(b > max) max = b;
 
-    // if(c > max) max = c;
+    if(c > max) max = c;
 
-    // if(d > max) max = d;
+    if(d > max) max = d;
 
-    // printf("Max value is: %d\n", max);
+    printf("Max value is: %d\n", max);
 
-    // int min = a;
+    int min = a;
 
-    // if(b < min) min = b;
+    if(b < min) min = b;
 
-    // if(c < min) min = c;
+    if(c < min) min = c;
 
-    // if(d < min) min = d;
+    if(d < min) min = d;
 
-    // printf("Min value is: %d\n", min);
+    printf("Min value is: %d\n", min);
 
     // Exercise 3: Write a program that prompts the user to input three positive integers ($a, b, c$). Determine if these three numbers can form a triangle. (The condition for a triangle is that the sum of any two sides must be greater than the remaining side). If they do form a triangle, calculate the perimeter and area; otherwise, display a message stating that they do not form a triangle.
 
-    // int a = 0;
-    // int b = 0;
-    // int c = 0;
-    // float p = 0;
-    // int perimeter = 0;
-    // float acreage = 0;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    float p = 0;
+    int perimeter = 0;
+    float acreage = 0;
 
-    // printf("Enter the value A: ");
-    // scanf("%d", &a);
+    printf("Enter the value A: ");
+    scanf("%d", &a);
 
-    // printf("Enter the value B: ");
-    // scanf("%d", &b);
+    printf("Enter the value B: ");
+    scanf("%d", &b);
 
-    // printf("Enter the value C: ");
-    // scanf("%d", &c);
+    printf("Enter the value C: ");
+    scanf("%d", &c);
 
-    // perimeter = a + b + c;
+    perimeter = a + b + c;
 
-    // printf("The perimeter of the triangle is: %d\n", perimeter);
+    printf("The perimeter of the triangle is: %d\n", perimeter);
 
-    // p = (float)perimeter / 2;
-    // acreage = sqrt(p * (p - a) * (p - b) * (p - c));
+    p = (float)perimeter / 2;
+    acreage = sqrt(p * (p - a) * (p - b) * (p - c));
 
-    // printf("The area of the triangle is: %.2f\n", acreage);
+    printf("The area of the triangle is: %.2f\n", acreage);
 
-    // if(a + b > c && a + c > b && b + c > a){
-    //     printf("This is a triangle!\n");
-    // }
-    // else{
-    //     printf("These numbers do NOT form a triangle.\n");
-    // }
+    if(a + b > c && a + c > b && b + c > a){
+        printf("This is a triangle!\n");
+    }
+    else{
+        printf("These numbers do NOT form a triangle.\n");
+    }
 
     // Exercise 4: Write a program that prompts the user to input a specific time in hours, minutes, and seconds (hh:mm:ss). Then, input an additional number of seconds to be added. Finally, calculate and display the resulting time in the standard format: hh:mm:ss.
 
-    // int time = 0;
-    // int hours = 0;
-    // int seconds = 0;
-    // int minutes = 0;
-    // int additional_seconds = 0;
-    // int totalSeconds = 0;
+    int time = 0;
+    int hours = 0;
+    int seconds = 0;
+    int minutes = 0;
+    int additional_seconds = 0;
+    int totalSeconds = 0;
 
-    // printf("Enter Hours: ");
-    // scanf("%d", &hours);
+    printf("Enter Hours: ");
+    scanf("%d", &hours);
 
-    // printf("Enter minutes: ");
-    // scanf("%d", &minutes);
+    printf("Enter minutes: ");
+    scanf("%d", &minutes);
 
-    // printf("Enter seconds: ");
-    // scanf("%d", &seconds);
+    printf("Enter seconds: ");
+    scanf("%d", &seconds);
 
-    // printf("Enter additional seconds: ");
-    // scanf("%d", &additional_seconds);
+    printf("Enter additional seconds: ");
+    scanf("%d", &additional_seconds);
 
-    // totalSeconds = hours * 3600 + minutes * 60 + seconds + additional_seconds;
-    // hours = (totalSeconds / 3600) % 24;
-    // minutes = (totalSeconds / 60) % 60;
-    // seconds = totalSeconds % 60;
+    totalSeconds = hours * 3600 + minutes * 60 + seconds + additional_seconds;
+    hours = (totalSeconds / 3600) % 24;
+    minutes = (totalSeconds / 60) % 60;
+    seconds = totalSeconds % 60;
 
-    // printf("Resulting time: %02d:%02d:%02d\n", hours, minutes, seconds);
+    printf("Resulting time: %02d:%02d:%02d\n", hours, minutes, seconds);
 
     // Exercise 5
 
