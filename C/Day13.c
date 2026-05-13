580c978a828b4d4f9e9104e32c7c44e917253514
ZeiGrin <buiphuoctoan707@gmail.com>
2026-05-13 20:42:39 +0700

    Complete Day 13, Day 14 Learning C Programming .ZGC

---
+++ C/Day13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    // LESSON 29: Arrays and User input
+
+    int scores[5];
+
+    for(int i = 0; i < 5; i++){
+        printf("%d ", scores[i]);
+        
+    }
+
+    return 0;
+}
---
+++ C/Day14.c
@@ -0,0 +1,78 @@
+// DAY 14 LEARNING C PROGRAMMING
+#include <stdio.h>
+
+int main(){
+
+    // LESSON 29: Array and User input (Review the old lesson)
+
+    // int scores[] = {100, 90, 80, 70, 60};
+    // int scores[5] = {0};
+
+    // printf("Enter a score: ");
+    // scanf("%d", &scores[0]);
+
+    // printf("Enter a score: ");
+    // scanf("%d", &scores[1]);
+
+    // printf("Enter a score: ");
+    // scanf("%d", &scores[2]);
+
+    // printf("Enter a score: ");
+    // scanf("%d", &scores[3]);
+
+    // printf("Enter a score: ");
+    // scanf("%d", &scores[4]);
+
+    // for(int i = 0; i < 5; i++){
+    //     printf("Enter a score: ");
+    //     scanf("%d", &scores[i]);
+    // }
+
+    // for(int i = 0; i < 5; i++){
+    //     printf("%d ", scores[i]);
+    // }
+
+    // LESSON 30: 2D Arrays
+
+    // int numbers[][3] = {{1, 2, 3},
+    //                     {4, 5, 6},
+    //                     {7, 8, 9},
+    //                     {10, 11, 12}};
+
+    // char numpad[][3] = {{'1', '2', '3'},
+    //                     {'4', '5', '6'},
+    //                     {'7', '8', '9'},
+    //                     {'*', '0', '#'}};
+
+    // printf("%d ", numbers[0][0]);
+    // printf("%d ", numbers[0][1]);
+    // printf("%d\n", numbers[0][2]);
+
+    // printf("%d ", numbers[1][0]);
+    // printf("%d ", numbers[1][1]);
+    // printf("%d\n", numbers[1][2]);
+
+    // printf("%d ", numbers[2][0]);
+    // printf("%d ", numbers[2][1]);
+    // printf("%d\n", numbers[2][2]);
+
+    // for(int i = 0; i < 4; i++){
+    //      for(int j = 0; j < 3; j++){
+    //         printf("%d ", numbers[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // for(int i = 0; i < 4; i++){
+    //     for(int j = 0; j < 3; j++){
+    //         printf("%c ", numpad[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // LESSON 31: Array of Strings
+
+    
+
+    return 0;
+}
