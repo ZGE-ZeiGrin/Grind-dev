809686fc8adef5d3e66e1680d56440f916bf8105
ZeiGrin <buiphuoctoan707@gmail.com>
2026-05-20 10:46:34 +0700

    Update Day 16 .ZGC

---
+++ C/Day16.c
@@ -0,0 +1,59 @@
+// DAY 16 LEARNING C PROGRAMMING
+#include <stdio.h>
+#include <stdbool.h>
+
+// typedef int Number;
+// typedef char String[50];
+// typedef char Initials[10];
+
+int main(){
+
+    // LESSON 33: Ternary Operator
+
+    // int x = 5;
+    // int y = 6;
+    // int max = (x > y) ? x : y;
+
+    // printf("%d", max);
+
+    // bool isOnline = false;
+
+    // printf("%s", (isOnline) ? "online" : "offline");
+
+    // int number = 8;
+
+    // printf("%d is %s", number, (number % 2 == 0) ? "even" : "odd");
+
+    // int age = 19;
+
+    // printf("%s", (age > 18) ? "adult" : "child");
+
+    // int hours = 12;
+    // int minutes = 30;
+    // char *meridiem = (hours > 12) ? "AM" : "PM";
+
+    // printf("%d:%d %s", hours, minutes, meridiem);
+
+    // LESSON 34: Typedef
+
+    // Number x = 3;
+    // Number y = 4;
+    // Number z = x + y;
+
+    // printf("%d", z);
+
+    // String name = "Grind";
+
+    // Initials user1 = "ZGE";
+    // Initials user2 = "ZGC";
+    // Initials user3 = "ZG";
+    // Initials user4 = "Gr";
+
+    // // printf("%s", name);
+    // printf("%s\n", user1);
+    // printf("%s\n", user2);
+    // printf("%s\n", user3);
+    // printf("%s\n", user4);
+
+    return 0;
+}
